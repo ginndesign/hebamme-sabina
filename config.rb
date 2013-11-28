7c65fda815e582365bb78752a2579f58e07a5819
ginndesign <julien@ginndesign.ch>
2013-11-28 17:31:35 +0100

    Font-Smoothing

--- config.rb
+++ config.rb
@@ -12,7 +12,7 @@ jade_dir = "jade"
 output_style = :nested
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
