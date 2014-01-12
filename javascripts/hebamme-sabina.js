3f60595de1728feb6aacce44c1740ae8eccb5a73
ginndesign <julien@ginndesign.ch>
2014-01-12 21:30:55 +0100

    Gesammelt in einem js-file

--- javascripts/hebamme-sabina.js
+++ javascripts/hebamme-sabina.js
@@ -26,3 +26,19 @@ var parallax = new Parallax(scene, {
 	frictionX: 0.01,
 	frictionY: 0.01
 });
+
+// Smooth scrolling
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
